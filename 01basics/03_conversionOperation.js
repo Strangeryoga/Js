ddf0e746c070de3b6ff4fd699c3b9f30cb909dfe
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-10-07 20:49:55 +0000

    created conversion operation file

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score= "tejas"
+
+// console.log(typeof score); //string
+// console.log(typeof(score)); //string
+
+//https://www.w3schools.com/js/js_typeof.asp
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN
+
+
+// "33" ==>  33
+// "883ae" =>  NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "tejas"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn); //true
+
+// 1 => true; 0 => false
+// "" => false 
+// "tejas" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber); //33
+// console.log(typeof stringNumber); //string
