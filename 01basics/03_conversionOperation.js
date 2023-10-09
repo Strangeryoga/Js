b93dc2d9569c91a9368a2e131144da4cd6234029
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-10-09 07:38:33 +0000

    added operations

--- 01basics/03_conversionOperation.js
+++ 01basics/03_conversionOperation.js
@@ -6,12 +6,14 @@ let score= "tejas"
 //https://www.w3schools.com/js/js_typeof.asp
 
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber); //number
-// console.log(valueInNumber); //NaN
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //NaN
 
-
+// "tejas" => NaN
 // "33" ==>  33
 // "883ae" =>  NaN
+// null => 0
+// undefined => NaN
 // true => 1; false => 0
 
 
@@ -28,3 +30,47 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber); //33
 // console.log(typeof stringNumber); //string
+
+
+
+// ************************* Operations *********************** //
+
+
+
+let value=3
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2); 
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "tejas"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2); 12
+// console.log(1+"2"); 12
+// console.log("1" + 2 + 2); 122
+// console.log(1 + 2 + "2"); 32
+
+// console.log((3 + 4) * 5 % 3); 2
+
+// console.log(+true); 1
+// console.log(+false); 0
+// console.log(+""); 0
+
+let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+// this is not proper 
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
