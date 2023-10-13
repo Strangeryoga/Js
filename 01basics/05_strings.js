107e7f8b9133fcfcbe998a0478ec9728088550d0
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-10-13 18:20:47 +0000

    added string

--- 01basics/05_strings.js
+++ 01basics/05_strings.js
@@ -11,3 +11,25 @@ const gameName = new String('stranger_yoga')
 // console.log(gameName[0]); s
 // console.log(gameName.__proto__);  {}
 
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
