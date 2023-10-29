9784f2d7c430f405125dba0219c3e8d4a4271dd5
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-10-29 15:23:50 +0000

    updated iife file

--- 03basics/04_iife.js
+++ 03basics/04_iife.js
@@ -1,11 +1,25 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+// To avoid the problems happened because of global scope pollution
+// To remove the var,fnc which is declared in global scope 
 
 (function chai(){
-    // named IIFE
-    console.log(`DB CONNECTED`);
+    // it is also called named IIFE
+    console.log(`DB CONNECTED`); // DB CONNECTED
 })();
+// here if don't write ; we will be getting error (TypeError: (intermediate value)(...) is not a function)
 
+
+( () => {
+    console.log(`DB CONNECTED TWO`); // DB CONNECTED TWO
+} )();
+
+
+// arrow fnc inside iife
 ( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('stranger')
+    // It is called simple IIFE
+    console.log(`DB CONNECTED THREE ${name}`); // DB CONNECTED THREE
+} )('stranger');
+
+
+// () here we write fnc definition
+// () execution call
