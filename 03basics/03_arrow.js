ad8bb7b44f5e4d90145764254afa64849a7ae26d
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-10-29 15:10:44 +0000

    updated arrow file

--- 03basics/03_arrow.js
+++ 03basics/03_arrow.js
@@ -1,52 +1,65 @@
+// this refers to current context
 const user = {
-    username: "hitesh",
+    username: "stranger",
     price: 999,
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        // console.log(this); //gives the value which is present inside user
     }
 
 }
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
 
-// console.log(this);
+// user.welcomeMessage() //stranger, welcome to website
+// user.username = "sam" 
+// user.welcomeMessage() //sam, welcome to website
+
+// console.log(this); //{} if we are in node we will be getting empty array
+// But if we try to print this inside browser console we well be getting window objecte
+
 
 // function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
+//     let username = "stranger"
+//     console.log(this) // Object [global]
+//     console.log(this.username); //undefined
 // }
-
 // chai()
+// if we try to print this inside fnc we will be getting Object [global]
+
 
 // const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
+//     let username = "stranger"
+//     console.log(this); // Object [global]
+//     console.log(this.username); //undefined
 // }
-
-const chai =  () => {
-    let username = "hitesh"
-    console.log(this);
-}
-
-
 // chai()
 
+
+// arrow function
+// const chai =  () => {
+//     let username = "stranger"
+//     console.log(this); // {}
+//     console.log(this.username) //undefined
+// }
+// chai()
+
+// aF
+// explicit return
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
 // }
 
+// implicit return
 // const addTwo = (num1, num2) =>  num1 + num2
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
+// If we wrap values inside currly braces then we have to write return
+// But if wrap it inside parenthesis() then we don't have to write
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
-
-
-console.log(addTwo(3, 4))
+// return object
+const addTwo = (num1, num2) => ({username: "stranger"})
+// console.log(addTwo(3, 4)) // {username: 'stranger'}
 
 
 // const myArray = [2, 5, 3, 7, 8]
