e1169184ef31260d7fe7e5cfae3b563c9bf687b7
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-10-31 07:19:39 +0000

    updated functions file

--- 03basics/01_functions.js
+++ 03basics/01_functions.js
@@ -94,3 +94,66 @@ function returnSecondValue(getArray){
 // console.log(returnSecondValue(myNewArray)); //300
 
 // console.log(returnSecondValue([799,933,231,982])); //231
+
+
+
+/* 1. What are Functions?
+
+Functions are at the heart of JavaScript and programming in general.
+They are reusable blocks of code that serve specific purposes or return values. 
+Functions are essential for structuring your code in an organized, modular, and efficient manner. 
+They allow you to break your code into smaller, manageable parts that can be used whenever needed.
+
+2. Declaring Functions:
+
+In JavaScript, you declare a function using the function keyword. 
+A function declaration typically includes:
+
+- The "function" keyword.
+- A function name that you choose (e.g., greet).
+- A set of parentheses that can hold parameters (input values).
+- A code block enclosed in curly braces {} that contains the instructions for what the function should do.
+
+3. Calling Functions:
+
+Once you've defined a function, you can call it to execute the code inside the function. 
+To call a function, simply use its name followed by parentheses. 
+If the function expects any arguments (parameters), you provide those within the parentheses.
+
+4. Function Parameters:
+
+Functions can receive parameters, which are like variables that hold values you pass into the function. 
+Parameters make functions versatile because you can pass different data to them when calling the function
+
+5. Return Statements:
+
+Functions can use the "return" statement to send a value back when they're called. 
+This value can be assigned to a variable or used directly
+
+6. Function Expressions:
+
+Functions can also be defined as expressions and assigned to variables. 
+These are known as function expressions. 
+Function expressions are useful when you need to pass functions as arguments to other functions or store them in data structures.
+
+7. Arrow Functions:
+
+Arrow functions provide a concise way to define functions, especially for simple, one-liner functions. 
+They are a shorthand for writing functions and are often used for anonymous functions.
+
+8. Function Scope:
+
+In JavaScript, variables declared inside a function are scoped to that function. 
+They are not accessible from outside the function, which is known as function scope. 
+This scoping behavior helps prevent variable name collisions and isolates variables within the context of the function.
+
+9. Function Declarations vs. Function Expressions:
+
+It's essential to understand the difference between function declarations and function expressions. 
+
+Function declarations are hoisted, meaning they are available throughout their scope, even before the declaration. 
+
+Function expressions, on the other hand, are not hoisted, and they can only be used after the point in code where they are defined.
+*/
+// https://pbs.twimg.com/media/F8T_zWsbIAA4sAA?format=jpg&name=large
+// https://pbs.twimg.com/media/F8T_zWvbYAAwQev?format=jpg&name=large
