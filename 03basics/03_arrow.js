05c6c0552ecba16d805a729a3e7040c26f875faf
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-10-31 07:10:07 +0000

    updated arrow file

--- 03basics/03_arrow.js
+++ 03basics/03_arrow.js
@@ -65,3 +65,47 @@ const addTwo = (num1, num2) => ({username: "stranger"})
 // const myArray = [2, 5, 3, 7, 8]
 
 // myArray.forEach()
+
+
+/* 1. What is this keyword?
+
+The "this" keyword in JavaScript is a special identifier that refers to the current execution context, specifically, the object that is currently executing the function.
+It is not determined by the scope in which the function is declared but rather by how the function is called.
+
+2. Key points about this and its relationship to scope:
+
+- this and Scope: 
+Unlike variables that are determined by lexical (static) scope, the value of this is not based on the function's position in the code. 
+It depends on the dynamic context in which the function is invoked.
+
+- Invocation Context: 
+The value of this is determined by how a function is called. There are several ways to call a function in JavaScript, and each way affects the value of this.
+
+3. Key aspects of the "this" keyword in more detail:
+
+- Dynamic Binding: 
+Unlike regular variables, the value of this is not determined by the lexical (static) scope where a function is defined. Instead, it's determined dynamically based on how the function is called at runtime.
+
+- Invocation Context: 
+The value of this is highly dependent on the context of the function's invocation. 
+Different ways of calling functions in JavaScript yield different values for this.
+
+- Global Context: 
+In the global context (i.e., not within a function or object), this refers to the global object. 
+In a web browser, this is typically the window object. In Node.js, it's the global object.
+
+- Method Context: 
+When a function is invoked as a method of an object, this refers to the object itself. 
+This is particularly useful for object-oriented programming.
+
+- Event Handlers: 
+In event handlers, such as those used in web development, this typically refers to the DOM element that triggered the event.
+
+- Constructor Functions: 
+When a function is used as a constructor with the new keyword, this refers to the newly created instance.
+
+- Arrow Functions: 
+Arrow functions have a different behavior regarding this. They do not have their own "this" binding but instead capture the "this" value from the enclosing lexical (static) scope.
+*/
+
+// https://pbs.twimg.com/media/F9hUqKFbUAAJ9ez?format=jpg&name=900x900
