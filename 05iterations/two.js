81d71f436596e1052527da6423ea331068692d78
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-11-14 03:39:11 +0000

    changes

--- 05iterations/two.js
+++ 05iterations/two.js
@@ -8,13 +8,13 @@ let myArray = ['flash', "batman", "superman"]
 
 let arr = 0
 while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
+    // console.log(`Value is ${myArray[arr]}`); // Value is flash, batman, superman
     arr = arr + 1
 }
 
 let score = 11
 
 do {
-    console.log(`Score is ${score}`);
+    console.log(`Score is ${score}`); // Score is 11
     score++
 } while (score <= 10);
