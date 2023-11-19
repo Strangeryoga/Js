dd6fbc2ee19dc8ee245b69f99e9c7f799aef29a4
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-11-19 18:18:32 +0000

    alphabetical order

--- JSQuestions/8.js
+++ JSQuestions/8.js
@@ -1,2 +1,7 @@
 // Write a Js fnc that return a passed string with letters in alphabetical order
 
+function stringAlpha(str){
+    return str.split("").sort().join("");
+}
+
+console.log(stringAlpha('apple'));
