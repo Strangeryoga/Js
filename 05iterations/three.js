918e4e88fe56f1794b80e19f70558a376ae1e2f4
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2023-11-21 08:12:41 +0000

    for of loop

---
+++ 05iterations/three.js
@@ -0,0 +1,59 @@
+// for of
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    // console.log(num); // 1 2 3 4 5
+}
+
+const welcome = "Hello World"
+for (const w of welcome){
+    // console.log(`char is ${w}`);
+}
+// o/p
+/* char is H
+char is e
+char is l
+char is l
+char is o
+char is  
+char is W
+char is o
+char is r
+char is l
+char is d
+*/
+
+// Maps
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "America")
+map.set('FR', "France")
+map.set('IN', "India")
+
+// console.log(map); // Map(3) { 'IN' => 'India', 'USA' => 'Ameriac', 'FR' => 'France' }
+
+for (const [key, value] of map) {
+    // console.log(key, ':', value);
+}
+// o/p
+/* IN : India
+USA : Ameriac
+FR : France
+*/
+
+const myObj = {
+    'h1': 'Batman',
+    'h2': 'Superman'
+}
+
+for ( const [key, value] of myObj){
+    // console.log(key, ':', value); // TypeError: myObj is not iterable
+}
